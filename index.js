e1dcb67ee2359d5579a1c7c091738c4c71e558bc
Abdullahi <abdullahiibrashair@gmail.com>
2025-03-08 13:20:14 +0300

    phase-1-first-class-functions

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callback) {
+    callback();
+}
+function returnsANamedFunction(){
+    function namedfunction(){
+
+    }
+    return namedfunction;
+}
+function returnsAnAnonymousFunction() {
+    return function(){
+
+    };
+    
+}
